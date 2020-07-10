62f6b1eff17dd8e9ac7d4562bf31b275fe254d0c
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-10 17:04:10 +0900

    server: 요청에 쿠키포함 허용

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true}))
 
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(cors('http://localhost:3000'))
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
 
 console.log(config.mongoURI)
 
